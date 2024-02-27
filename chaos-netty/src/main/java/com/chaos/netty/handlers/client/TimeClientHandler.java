a3113d35fa2098dba3a695777e0230bd7128c22d
tangzw <724144090@qq.com>
2024-02-27 18:02:31 +0800

    fix

--- chaos-netty/src/main/java/com/chaos/netty/handlers/client/TimeClientHandler.java
+++ chaos-netty/src/main/java/com/chaos/netty/handlers/client/TimeClientHandler.java
@@ -18,7 +18,7 @@ public class TimeClientHandler extends DefaultThrowHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf m = (ByteBuf) msg;
         try {
-            long currentTimeMillis = (m.readUnsignedInt() - 2208922200L) * 1000L;
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(currentTimeMillis));
             ctx.close();
         } finally {
